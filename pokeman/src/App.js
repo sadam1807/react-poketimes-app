e68f710f17dee6aaf6ff053e9168dd09120455b6
sadam <sanjeet.kanekar@sjinnovation.com>
2018-12-07 13:10:57 +0530

    first commit

---
+++ pokeman/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+    
+      </div>
+    );
+  }
+}
+
+export default App;
