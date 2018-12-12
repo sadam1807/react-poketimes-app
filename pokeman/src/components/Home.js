835b4bc4395830bc72e3829666eacd2b8c6a6f05
sadam1807 <sadam.bapunawar@gmail.com>
2018-12-12 15:41:22 +0530

    map state to props

--- pokeman/src/components/Home.js
+++ pokeman/src/components/Home.js
@@ -1,25 +1,10 @@
 import React, { Component }  from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class  Home extends Component {
-    
-    state = {
-        posts: []
-    }
-
-    componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(res => {
-            this.setState({
-                posts: res.data.slice(0,10)
-            })
-        });
-
-    }
-
     render(){
-    const { posts }  = this.state;
+    const { posts }  = this.props;
     const postList = posts.length ? (posts.map(post => {
          return(
              <div className="post card" key={post.id}>
@@ -46,4 +31,9 @@ class  Home extends Component {
     }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+export default connect(mapStateToProps)(Home)
--- pokeman/src/reducers/rootReducers.js
+++ pokeman/src/reducers/rootReducers.js
@@ -1,5 +1,8 @@
 const initState = {
-    posts: []
+    posts: [
+        { id: '1', title: 'Squired Laid an Egg', body: 'Lorem ipsum' },
+        { id: '2', title: 'Squired Laid  Egg', body: 'Lorem ipsum dfgf'}
+    ]
 }
 
 const rootReducer = (state = initState, action) => {
